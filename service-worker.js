852518336ea6588cb557d25a2a3795dd2b21cebb
Martin Davico <marthindavico@gmail.com>
2022-04-01 15:10:23 -0300

    finished banner

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,10 @@
-const cacheName = 'nirdine-v1';
+const cacheName = 'nirdine-v3';
 const appShellFiles = [
-    './'
+  
 ];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(appShellFiles);
-  })());
-});
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
+
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then((keyList) => {
